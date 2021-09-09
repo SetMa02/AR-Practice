0b0bfa5f3267bbdce7677a45920c1e2a991f45d1
Максим Майнагашев <maxuser99@mail.ru>
2021-09-09 14:49:09 +0700

    Shooting added

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTempalte;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_bulletTempalte, _shootPoint);
+        }
+    }
+}
